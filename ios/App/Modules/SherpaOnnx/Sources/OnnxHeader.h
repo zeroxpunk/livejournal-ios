4fd8c8a7ae20df515b239e96788f0f165efb3657
zeroxpunk <punk.dvpn@proton.me>
2025-08-20 18:40:56 +0300

    Added SherpaOnnx c-api

---
+++ ios/App/Modules/SherpaOnnx/Sources/OnnxHeader.h
@@ -0,0 +1,13 @@
+//
+//  OnnxHeader.h
+//  SpeechDiarizationStarter
+//
+//  Created by Carlos Mbendera on 27/02/2025.
+//
+
+#ifndef OnnxHeader_h
+#define OnnxHeader_h
+
+#import "Headers/sherpa-onnx/c-api/c-api.h"
+
+#endif /* OnnxHeader_h */
